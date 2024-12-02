549f0ff086826658b6073938a4b9fd463b241cc5
Mijael Rodrigo Vargas Godoy <mirovago@gmail.com>
2024-12-02 12:40:31 +0000

    version 0.0.1

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c(".", "A", "B", "C", "coord_id", "D", "dataset",
+                         "forecast", "idx", "lat", "lon", "observed", "seasons",
+                         "t1", "t2", "value"))
---
+++ R/sellonlatbox.R
@@ -0,0 +1,49 @@
+#' Select Longitude Latitude Box
+#'
+#' The function \code{sellonlatbox} subsets the data in space within a bounding box.
+#'
+#' @details
+#' If `x` is a data.table, its columns should be named: "lon", "lat", "date", and "value"
+#'
+#' If `x` is a filename, it should point to a *.nc file.
+#'
+#' @import data.table
+#' @importFrom methods setGeneric setMethod
+#' @importFrom raster brick crop extent
+#' @param x Raster* object; data.table (see details); filename (character, see details)
+#' @param y numeric. Bounding box in the form: (xmin, xmax, ymin, ymax)
+#' @return Raster* object; data.table
+#' @keywords internal
+
+setGeneric("sellonlatbox", function(x, y) standardGeneric("sellonlatbox"))
+
+#' @rdname sellonlatbox
+#' @method sellonlatbox Raster
+
+setMethod("sellonlatbox", "Raster",
+          function(x, y) {
+            lonlatbox <- extent(y[1], y[2], y[3], y[4])
+            dummie <- crop(x, lonlatbox)
+            return(dummie)
+          })
+
+#' @rdname sellonlatbox
+#' @method sellonlatbox data.table
+
+setMethod("sellonlatbox", "data.table",
+          function(x, y) {
+            dummie <- x[(lon >= y[1]) & (lon <= y[2]) &
+                                     (lat >= y[3]) & (lat <= y[4])]
+            return(dummie)
+          })
+
+#' @rdname sellonlatbox
+#' @method sellonlatbox character
+
+setMethod("sellonlatbox", "character",
+          function(x, y) {
+            dummie_brick <- brick(x)
+            lonlatbox <- extent(y[1], y[2], y[3], y[4])
+            dummie <- crop(dummie_brick, lonlatbox)
+            return(dummie)
+          })
