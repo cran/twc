cd29ec1f834a4806af4fe74ddc57ea20b666afba
Mijael Rodrigo Vargas Godoy <mirovago@gmail.com>
2025-04-02 15:50:02 +0000

    version 0.0.2

--- R/fldmean.R
+++ R/fldmean.R
@@ -4,9 +4,9 @@
 #'
 #' @details
 #' If `x` is a data.table, its columns should be named: "lon", "lat", "date", and "value"
-#' 
+#'
 #' If `x` is a filename, it should point to a *.nc file.
-#' 
+#'
 #' @import data.table doParallel foreach parallel sp
 #' @importFrom methods setGeneric setMethod
 #' @importFrom raster area brick cellStats getZ
@@ -32,8 +32,12 @@ setMethod("fldmean", "Raster",
             if (no_cores < 1 | is.na(no_cores))(no_cores <- 1)
             registerDoParallel(cores = no_cores)
             dummie_dates <- getZ(x) %>% aux_date()
+            dummie_rows <- x@file@blockrows
+            dummie_cols <- x@file@blockcols
             dummie <- foreach (idx = 1:nlayers(x), .combine = rbind) %dopar% {
               dummie_step <- x[[idx]]
+              dummie_step@file@blockrows <- dummie_rows
+              dummie_step@file@blockcols <- dummie_cols
               dummie_area <- area(dummie_step, na.rm = TRUE, weights = TRUE)
               dummie_step <- dummie_area * dummie_step
               dummie_step <- cellStats(dummie_step, stat = "sum", na.rm = TRUE)
